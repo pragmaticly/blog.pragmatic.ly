fb9a45f5c1c3197afbaef9b210827a867442a2bd
Dingding Ye <yedingding@gmail.com>
2014-10-31 02:22:12 +0800

    move gif file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-middleman build
+bundle exec middleman build
 cd build && git add . && git commit -m "update" && git push -f origin gh-pages
 cd ../
-BLOG_PREFIX=true middleman build
+BLOG_PREFIX=true bundle exec middleman build
 cd build && git add . && git commit -m "update" && git push -f origin gh-pages:internal
 cd ../
